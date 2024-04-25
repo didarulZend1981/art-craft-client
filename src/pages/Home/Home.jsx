be090c970842d0e3aec514c84e4d8feb90562689
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-04-26 05:23:34 +0600

    toggle dark & light

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar/NavBar";
+
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <h2>Home page</h2>
+    </div>
+  );
+};
+
+export default Home;
