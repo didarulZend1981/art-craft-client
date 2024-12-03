eab4ea48f06401098fa09515b0c752cb5674154f
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-12-03 13:39:18 +0600

    category section

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -7,11 +7,11 @@ const CategoryCard = ({ label, image }) => {
 
 
     <div className="card w-full bg-base-100 shadow-xl border-solid border-2 border-[wheat]">
-              <figure className="px-10 pt-10">
-                <img src={image} alt={label} className="rounded-xl" />
+              <figure className="px-2 pt-1">
+                <img src={image} alt={label} className="rounded-xl h-[100px]" />
               </figure>
-              <div className="card-body items-center text-center">
-                <h2 className="card-title">{label}</h2>
+              <div className="px-2 py-2 items-center text-center">
+                <h2 className="">{label}</h2>
                
                
               </div>
--- src/components/CategorySection/CategorySection.jsx
+++ src/components/CategorySection/CategorySection.jsx
@@ -4,16 +4,17 @@ const CategorySection = () => {
   return (
  
     <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8">
-          <CategoryCard label="Shoes!" image="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"/>
+          <CategoryCard label="Wooden Furniture & Sculptures" image="https://i.ibb.co.com/10Ms7yh/Wooden-Furniture-Sculptures.webp"/>
+          
 
-          <CategoryCard label="Shoes!" image="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"/>
+          <CategoryCard label="Jute and Wooden Jewelry!" image="https://i.ibb.co.com/72SMyQ4/Jute-and-Wooden-Jewelry.webp"/>
 
-          <CategoryCard label="Shoes!" image="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"/>
+          <CategoryCard label="Wooden Home Decor!" image="https://i.ibb.co.com/LksLprT/Wooden-Home-Decor.webp"/>
 
-          <CategoryCard label="Shoes!" image="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"/>
-          <CategoryCard label="Shoes!" image="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"/>
+          <CategoryCard label="Wooden Utensils and Kitchenware!" image="https://i.ibb.co.com/nQc6SXb/Wooden-Utensils-and-Kitchenware.webp"/>
+          <CategoryCard label="Jute Home Decor!" image="https://i.ibb.co.com/bPvh6xJ/Jute-Home-Decor.webp"/>
 
-          <CategoryCard label="Shoes!" image="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"/>
+          <CategoryCard label="Jute Kitchenware & Utensils" image="https://i.ibb.co.com/r4hSmQX/Jute-Kitchenware-Utensils.webp"/>
            
     </div>
   
